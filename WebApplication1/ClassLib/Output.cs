77725bd7fc4eafb538dd24956e04e92fc5ecf9ad
EkaterinaPuzyrenko0903 <101350575+EkaterinaPuzyrenko0903@users.noreply.github.com>
2023-06-07 18:05:59 +0500

    07/06/23
    Правка_2

--- WebApplication1/ClassLib/Output.cs
+++ WebApplication1/ClassLib/Output.cs
@@ -14,18 +14,12 @@ namespace ClassLib
         public double Nu_sh_k { get; set; }
         public double ps { get; set; } //параметр
         public double s { get; set; } //эффективная толщина излучающего слоя
-        public double d { get; set; } //Диаметр
-        public double S1 { get; set; }  //Поперечный шаг
-        public double S2 { get; set; } // Продольный шаг
-        public double w { get; set; }   //Скорость потока
-        public double t { get; set; }   //Средняя температура потока
-        public double k_e { get; set; }
+        public double k_e { get; set; } //Коэффициент поглощения газового объема
         public double k_l { get; set; }
-        public double e_d { get; set; }
+        public double e_d { get; set; } //Степень черноты газов
         public double a_d { get; set;}
-        public double c_pr { get; set; }
-        public double a_dl { get; set;}
-   
+        public double c_pr { get; set; } //Приведенный коэффициент излучения
+        public double a_dl { get; set;} //Коэффициент теплоотдачи излучением
 
     }
 }
